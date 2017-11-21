a5a6b2c4ad174d08de952bd8b8c638d41e3347bc
Beando <Flaygames1610@gmail.com>
2017-11-21 11:39:05 +0000

    Initial changes

---
+++ Retro/sketch.js
@@ -0,0 +1,118 @@
+//https://p5js.org
+//https://p5js.org/learn/
+
+//DAT405 / GAD405
+//00_Template
+//The purpose of this sketch is to provide a basic template
+//for P5.js projects
+
+//Initialization function
+function setup() {
+  //Print a message to the console - to view using Chrome:
+  //View > Developer > Developer Tools > Console
+  console.log("Initialization: OK")
+  createCanvas(700,700);
+}
+
+//Rendering function
+function draw() {
+  //Set a background color
+  background(255);
+  stroke(0)
+  //row 1
+  rect(0,0,40,40)
+  rect(40,0,40,40)
+  rect(80,0,40,40)
+  rect(120,0,40,40)
+  fill(255,4,4)
+  rect(160,0,40,40)
+  rect(200,0,40,40)
+  rect(240,0,40,40)
+  rect(280,0,40,40)
+  rect(320,0,40,40)
+  noFill()
+  rect(360,0,40,40)
+  rect(400,0,40,40)
+  rect(440,0,40,40)
+  rect(480,0,40,40)
+  rect(520,0,40,40)
+  rect(560,0,40,40)
+  //row 2
+  rect(0,40,40,40)
+  rect(40,40,40,40)
+  rect(80,40,40,40)
+  fill(255,4,4)
+  rect(120,40,40,40)
+  rect(160,40,40,40)
+  rect(200,40,40,40)
+  rect(240,40,40,40)
+  rect(280,40,40,40)
+  rect(320,40,40,40)
+  rect(360,40,40,40)
+  rect(400,40,40,40)
+  rect(440,40,40,40)
+  noFill()
+  rect(480,40,40,40)
+  rect(520,40,40,40)
+  rect(560,40,40,40)
+  //row 3
+  rect(0,80,40,40)
+  rect(40,80,40,40)
+  rect(80,80,40,40)
+  //hair
+  fill(97,34,16)
+  rect(120,80,40,40)
+  rect(160,80,40,40)
+  rect(200,80,40,40)
+  noFill()
+  //skin
+  fill(233,162,74)
+  rect(240,80,40,40)
+  rect(280,80,40,40)
+  noFill()
+  fill(0)
+  rect(320,80,40,40)
+  noFill()
+  fill(233,162,74)
+  rect(360,80,40,40)
+  noFill()
+  rect(400,80,40,40)
+  rect(440,80,40,40)
+  rect(480,80,40,40)
+  rect(520,80,40,40)
+  rect(560,80,40,40)
+  //row 4
+  rect(0,120,40,40)
+  rect(40,120,40,40)
+  fill(97,34,16)
+  rect(80,120,40,40)
+  noFill()
+  fill(233,162,74)
+  rect(120,120,40,40)
+  noFill()
+  fill(97,34,16)
+  rect(160,120,40,40)
+  noFill()
+  fill(233,162,74)
+  rect(200,120,40,40)
+  rect(240,120,40,40)
+  rect(280,120,40,40)
+  noFill()
+  fill(0)
+  rect(320,120,40,40)
+  noFill()
+  fill(233,162,74)
+  rect(360,120,40,40)
+  rect(400,120,40,40)
+  rect(440,120,40,40)
+  noFill()
+  rect(480,120,40,40)
+  rect(520,120,40,40)
+  rect(560,120,40,40)
+  //Row 5
+  
+
+
+
+
+}
