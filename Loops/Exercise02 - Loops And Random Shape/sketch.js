2cd4a5da8ef2d7c6a2ef44c4afbf113fe3897a85
Beando <Flaygames1610@gmail.com>
2017-11-23 15:02:13 +0000

    Lesson 3

--- Loops/Exercise02 - Loops And Random Shape/sketch.js
+++ Loops/Exercise02 - Loops And Random Shape/sketch.js
@@ -12,20 +12,33 @@ function setup() {
 }
 
 function draw() {
+  ellipseMode(CORNER)
+
   //Run the nested loop for x, and y
-  for (let y = 0; y < 10; y++) {
-    for (let x = 0; x < 10; x++) {
+  for (let y = 0; y < 5; y++) {
+    for (let x = 0; x < 5; x++) {
       //Draw the background shapes
       fill(215, 200, 170);
+      console.log(x%2)
       if (x%2) {
+        if (y%2){
         ellipse(size * x, size * y, size, size);
         rect(12.5+size * x, 12.5+size * y, size/2, size/2);
       }
       else {
-
         rect(size * x, size * y, size, size)
-        ellipseMode(CORNER)
         ellipse(12.5+size * x, 12.5+size * y, size/2, size/2);
+       }
+      }
+      else {
+        if (y%2) {
+          rect(size * x, size * y, size, size)
+          ellipse(12.5+size * x, 12.5+size * y, size/2, size/2);
+        }
+        else {
+          ellipse(size * x, size * y, size, size);
+          rect(12.5+size * x, 12.5+size * y, size/2, size/2);
+        }
       }
 
       //Draw the foreground shapes with a random RGB color
