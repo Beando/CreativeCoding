911057a55bc61f5b357a82d982e27af7ff91d1ef
Beando <Flaygames1610@gmail.com>
2017-11-27 16:21:48 +0000

    Initila commit

--- 1 - Tasks/4 - Interactive Colour Grid/sketch.js
+++ 1 - Tasks/4 - Interactive Colour Grid/sketch.js
@@ -15,11 +15,25 @@ function draw() {
   background(0); //Set your background color to black (0)
 
   //code here...
-  for (let y = 0; y < 10; y++) {
-    for (let x = 0; x < 10; x++) {
+  var mouse1 = map(mouseY,0,height,0,255)
+  var mouse2 = map(mouseX,0,width,0,255)
+  var mouse3 = map(mouseY,0,width,255,0)
+
+  var color1 = color(255,37,54,255)
+  var color2 = color(36,194,203,mouse1)
+  var color3 = color(36,194,203,mouse2)
+  var color4 = color(239,108,46,mouse3)
+
+  for (let y = 0; y < height;y += tileSize) {
+    for (let x = 0; x < width;x += tileSize) {
       //Draw the background shapes
-      fill(215, 200, 170);
-      rect(tileSize * x, tileSize * y, tileSize, tileSize);
+      //fill(215, 200, 170);
+
+      if(mouseX>x && mouseX<x+tileSize && mouseY>y && mouseY<y+tileSize) fill(color1);
+      else if (mouseX>x && mouseX<x+tileSize) fill(color2);
+      else if (mouseY>y && mouseY<y+tileSize) fill(color3);
+      else fill(color4);
+      rect(x,y,tileSize,tileSize);
 }
 }
 
